c142758788f3cecb6e2688a591250adbefe4e34a
Hoa Do <LitlteMsFlorists@Gmail.com>
2022-12-28 06:34:49 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=': 
+                try {
+                    display.innerText = eval(display.innerText);
+                }
+
+                catch {
+                    display.innerText = "no! lol u dumb bish 💩 "
+                }
+
+                break;
+                
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
